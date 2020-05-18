73b4d6ddc0e781210cc05fa4a99eab2f1c6abd55
Victor Vollmann <vollmannv@gmail.com>
2020-05-18 14:49:55 +0200

    initial branch commit

---
+++ scenes/Scene4.js
@@ -0,0 +1,23 @@
+class Scene4 extends Phaser.Scene {
+
+    constructor() {
+        super("Level4");
+    }
+
+    init () {
+
+    }
+
+    preload () {
+
+    }
+
+    create () {
+
+    }
+
+    update () {
+
+    }
+
+}
